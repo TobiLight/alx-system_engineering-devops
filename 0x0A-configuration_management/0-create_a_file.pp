2c5a58af47c2c56fb1adf3be2ccee001508e1786
0x <jgetitdonefast@gmail.com>
2023-09-23 02:53:16 +0100

    Create 0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Create a File in /tmp directory
+
+file { 'holberton':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/holberton',
+}
