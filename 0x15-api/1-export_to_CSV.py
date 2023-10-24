9d01937df618c1f7e1f65c6b70cd2afa544193ec
TobiLight <jgetitdonefast@gmail.com>
2023-10-24 01:20:01 +0100

    Exports employee's todo list in csv format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,40 +1,39 @@
 #!/usr/bin/python3
-# File: 0-gather_data_from_an_API.py
+# File: 1-export_to_CSV.py
 # Author: Oluwatobiloba Light
 """
 Returns information about an employee's TODO list progress for a
-given employee ID.
+given employee ID and exports data in CSV format.
 """
 import requests
 import sys
+import csv
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         userID = sys.argv[1]
-        response = requests.\
-            get("https://jsonplaceholder.typicode.com/users/{}".
-                format(userID))
-        todo_response = requests.\
-            get("https://jsonplaceholder.typicode.com/todos")
 
         try:
-            response.raise_for_status()
-            todo_response.raise_for_status()
-            user = response.json()
-            todos = todo_response.json()
-            completed = 0
-            total = 0
-            todos_title = []
-            for todo in todos:
-                if str(todo['userId']) == userID:
-                    total += 1
-                if str(todo['userId']) == userID and todo['completed'] is True:
-                    completed += 1
-                    todos_title.append(todo['title'])
-            print("Employee {} is done with tasks({}/{}):".
-                  format(user['name'], completed, total))
-            for todo in todos_title:
-                print("\t {}".format(todo))
+            user = requests.\
+                get("https://jsonplaceholder.typicode.com/users/{}".
+                    format(userID))
+            user.raise_for_status()
+            user = user.json()
+            todos = requests.\
+                get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                    format(userID))
+            # raise an error if it occurs
+            todos.raise_for_status()
+            todos = todos.json()
+            filename = "{}.csv".format(userID)
+            with open(filename, 'w',  newline='', encoding="utf-8") as\
+                    file:
+                writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC)
+                for data in todos:
+                    writer.writerow([str(data['userId']),
+                                     str(user['username']),
+                                     str(data['completed']),
+                                     str(data['title'])])
         except requests.exceptions.RequestException as e:
             pass
